8a749d2627e177a01eed31eb88d365a1a4cf3c82
Warsame Jama <warsame.jama80>
2022-09-14 03:07:38 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,21 +2,22 @@
 /**
  * Function that prints the sign of a number
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
-		return(1);
+		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('48');
-		return(0);
+		_putchar('0');
+		return (0);
 	}
-	else (n < 0}
+	else
 	{
 		_putchar('-');
-		return(-1);
-		}
+		return (-1);
+	}
 }
